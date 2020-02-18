aad3fa9e91d1d8d5853cb41fd0f4ab9d9a43dee5
Zababurin Sergey <s.zababurin.v@gmail.com>
2020-02-18 07:35:05 +0300

    update

--- images.mjs
+++ images.mjs
@@ -1,3 +1,9 @@
-export default async ()=>{
-return JSON.parse([{"mimeType":"image/png","name":"problem.png","link":"https://drive.google.com/uc?id=1h3PfVGBNtoi9kek2XV6sRldTVS08m46I"},{"mimeType":"image/png","name":"ffff.png","link":"https://drive.google.com/uc?id=1gVyBVyzWKlylBG0_4qfR1H-RD2q9RVpa"}])
-}
+sdfsdf
+
+
+
+
+
+
+
+sdfsdf
